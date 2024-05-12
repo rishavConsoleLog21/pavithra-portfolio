7bf7c3e90679f54697546ef82720d445328def56
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-05-12 17:25:29 +0530

    + Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">Hi</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-pink-400" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-pink-400" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "text-pink-400" : "text-black"
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
